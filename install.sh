acf8f6bd2e40dde07341b737ea032507c1e1ff62
thunder <thunder@debianserver.thunderace.fr>
2015-09-29 20:01:23 +0200

    use last git esptool - the last stable release dont work with nodemcu

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
 wget https://github.com/igrr/mkspiffs/releases/download/0.1.1/mkspiffs-0.1.1-linux64.tar.gz && cd bin && tar xvfz ../mkspiffs-0.1.1-linux64.tar.gz --strip 1 && cd .. && rm mkspiffs-0.1.1-linux64.tar.gz
 wget http://arduino.esp8266.com/linux64-xtensa-lx106-elf-gb404fb9.tar.gz && tar xvfz linux64-xtensa-lx106-elf-gb404fb9.tar.gz && rm linux64-xtensa-lx106-elf-gb404fb9.tar.gz
-wget https://github.com/igrr/esptool-ck/releases/download/0.4.6/esptool-0.4.6-linux64.tar.gz && cd bin && tar xvfz ../esptool-0.4.6-linux64.tar.gz --strip 1 && cd .. && rm esptool-0.4.6-linux64.tar.gz
