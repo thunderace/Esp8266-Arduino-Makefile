b655fc264ba93f3d7e86b36d44dd31210e4cdcab
thunder <thunder@debianserver.thunderace.fr>
2016-06-22 10:07:47 +0200

    remove temp build dir

--- tests/mergeino/build.nodemcu/core/libb64/cdecode.c.d
+++
@@ -1,3 +0,0 @@
-build.nodemcu/core/libb64/cdecode.c.o: \
- /media/data/home/thunder/Esp8266-Arduino-Makefile/esp8266/cores/esp8266/libb64/cdecode.c \
- /media/data/home/thunder/Esp8266-Arduino-Makefile/esp8266/cores/esp8266/libb64/cdecode.h
--- tests/mergeino/build.nodemcu/core/umm_malloc/umm_malloc.c.d
+++
@@ -1,6 +0,0 @@
-build.nodemcu/core/umm_malloc/umm_malloc.c.o: \
- /media/data/home/thunder/Esp8266-Arduino-Makefile/esp8266/cores/esp8266/umm_malloc/umm_malloc.c \
- /media/data/home/thunder/Esp8266-Arduino-Makefile/esp8266/cores/esp8266/umm_malloc/umm_malloc.h \
- /media/data/home/thunder/Esp8266-Arduino-Makefile/esp8266/cores/esp8266/umm_malloc/umm_malloc_cfg.h \
- /media/data/home/thunder/Esp8266-Arduino-Makefile/esp8266/cores/esp8266/debug.h \
- /media/data/home/thunder/Esp8266-Arduino-Makefile/esp8266/tools/sdk/include/c_types.h
