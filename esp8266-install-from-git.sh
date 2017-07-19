90dc801f3d728b66db04ad0fb5b344d87ce5de31
thunder <thunder@debianserver.thunderace.fr>
2017-07-19 15:59:11 +0200

    forgot to commit this one

--- esp8266-install-from-git.sh
+++ esp8266-install-from-git.sh
@@ -1,15 +1,5 @@
-declare ESP8266_VER=2.3.0
+git clone https://github.com/esp8266/Arduino.git esp8266
 
-declare DOWNLOAD_CACHE=./download
-mkdir $DOWNLOAD_CACHE
-
-# Get Arduino core for ESP8266 chip
-wget --no-clobber https://github.com/esp8266/Arduino/releases/download/$ESP8266_VER/esp8266-$ESP8266_VER.zip -P $DOWNLOAD_CACHE
-unzip $DOWNLOAD_CACHE/esp8266-$ESP8266_VER.zip
-rm -f esp8266
-ln -s esp8266-$ESP8266_VER esp8266
 cd esp8266/tools && ./get.py
 
-#cleanup
-rm -fr $DOWNLOAD_CACHE
 
