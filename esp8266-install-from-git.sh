dec62d96111d4aab7238f95557111053dda282e8
thunder <thunder@debianserver.thunderace.fr>
2017-07-19 16:16:54 +0200

    esp8266 installation from git enhanced

--- esp8266-install-from-git.sh
+++ esp8266-install-from-git.sh
@@ -1,5 +1,6 @@
-git clone https://github.com/esp8266/Arduino.git esp8266
-
+git clone https://github.com/esp8266/Arduino.git esp8266.git
+rm -f esp8266
+ln -s esp8266.git esp8266
 cd esp8266/tools && ./get.py
 
 
