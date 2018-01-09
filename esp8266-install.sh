26d1168a0acb36acf89254dac66c37cf0a94c54d
thunder <thunder@debianserver.thunderace.fr>
2018-01-09 14:08:24 +0100

    update for release 2.4.0

--- esp8266-install.sh
+++ esp8266-install.sh
@@ -1,4 +1,4 @@
-declare ESP8266_VER=2.3.0
+declare ESP8266_VER=2.4.0
 
 declare DOWNLOAD_CACHE=./download
 mkdir $DOWNLOAD_CACHE
@@ -6,7 +6,8 @@ mkdir $DOWNLOAD_CACHE
 # Get Arduino core for ESP8266 chip
 wget --no-clobber https://github.com/esp8266/Arduino/releases/download/$ESP8266_VER/esp8266-$ESP8266_VER.zip -P $DOWNLOAD_CACHE
 unzip $DOWNLOAD_CACHE/esp8266-$ESP8266_VER.zip
-cp -R bin/package esp8266
+mkdir esp8266-$ESP8266_VER/package
+wget --no-clobber http://arduino.esp8266.com/versions/$ESP8266_VER/package_esp8266com_index.json -O esp8266-$ESP8266_VER/package/package_esp8266com_index.template.json
 cd esp8266-$ESP8266_VER/tools && ./get.py && cd ../..
 if [ "$OSTYPE" == "cygwin" ] || [ "$OSTYPE" == "msys" ]; then
 	chmod +x ./esp8266-$ESP8266_VER/tools/esptool/esptool.exe
