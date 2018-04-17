8b5192981162a2cf0973b191b5831f10259c2278
thunder <thunder@debianserver.thunderace.fr>
2018-04-17 11:05:47 +0200

    switch to lwip v2 Lower Memory - option to log minicom serial to file

--- espXArduino.mk
+++ espXArduino.mk
@@ -247,7 +247,7 @@ LIB_OBJ_FILES = $(addprefix $(BUILD_OUT)/libraries/,$(notdir $(ULIB_CSRC:.c=.c.o
 
 
 ifeq ($(ARDUINO_ARCH),esp8266)
-	CPREPROCESSOR_FLAGS = -D__ets__ -DICACHE_FLASH -U__STRICT_ANSI__ -I$(ESPRESSIF_SDK)/include -I$(ESPRESSIF_SDK)/lwip/include -I$(ESPRESSIF_SDK)/libc/xtensa-lx106-elf/include -I$(BUILD_OUT)/core
+	CPREPROCESSOR_FLAGS = -D__ets__ -DICACHE_FLASH -U__STRICT_ANSI__ -I$(ESPRESSIF_SDK)/include -I$(ESPRESSIF_SDK)/lwip2/include -I$(ESPRESSIF_SDK)/libc/xtensa-lx106-elf/include -I$(BUILD_OUT)/core
 else
 	CPREPROCESSOR_FLAGS = -DESP_PLATFORM -DMBEDTLS_CONFIG_FILE="mbedtls/esp_config.h" -DHAVE_CONFIG_H -I$(ESPRESSIF_SDK)/include/config \
 					-I$(ESPRESSIF_SDK)/include/bluedroid -I$(ESPRESSIF_SDK)/include/app_trace -I$(ESPRESSIF_SDK)/include/app_update -I$(ESPRESSIF_SDK)/include/bootloader_support \
@@ -262,7 +262,7 @@ else
 endif
 
 ifeq ($(ARDUINO_ARCH),esp8266)
-	DEFINES = -DF_CPU=$(F_CPU) -DLWIP_OPEN_SRC -DARDUINO=$(ARDUINO_VERSION) \
+	DEFINES = -DF_CPU=$(F_CPU) -DLWIP_OPEN_SRC -DTCP_MSS=536 -DARDUINO=$(ARDUINO_VERSION) \
 		-DARDUINO_$(ARDUINO_BOARD) -DARDUINO_ARCH_$(shell echo "$(ARDUINO_ARCH)" | tr '[:lower:]' '[:upper:]') \
 		-DARDUINO_BOARD=\"$(ARDUINO_BOARD)\" -DESP8266 
 else
@@ -284,7 +284,7 @@ ifeq ($(ARDUINO_ARCH),esp8266)
 		-falign-functions=4 -MMD -std=gnu99 -ffunction-sections -fdata-sections
 	CXXFLAGS = -c -Os -g -mlongcalls -mtext-section-literals -fno-exceptions \
 		-fno-rtti -falign-functions=4 -std=c++11 -MMD -ffunction-sections -fdata-sections
-	ELFLIBS = -lhal -lphy -lpp -lnet80211 -llwip_gcc -lwpa -lcrypto -lmain -lwps -laxtls -lespnow -lsmartconfig -lairkiss -lwpa2 -lstdc++ -lm -lc -lgcc
+	ELFLIBS = -lhal -lphy -lpp -lnet80211 -llwip2 -lwpa -lcrypto -lmain -lwps -laxtls -lespnow -lsmartconfig -lairkiss -lwpa2 -lstdc++ -lm -lc -lgcc
 	ELFFLAGS = -g -Os -nostdlib -Wl,--no-check-sections -u call_user_start -u _printf_float -u _scanf_float -Wl,-static \
 			-L$(ESPRESSIF_SDK)/lib -L$(ESPRESSIF_SDK)/ld -L$(ESPRESSIF_SDK)/libc/xtensa-lx106-elf/lib \
 			 -T$(FLASH_LD) \
@@ -452,10 +452,18 @@ ota: $(BUILD_OUT)/$(TARGET).bin
 	$(ESPOTA) -i $(OTA_IP) -p $(OTA_PORT) -a $(OTA_AUTH) -f $(BUILD_OUT)/$(TARGET).bin
 
 term:
+ifeq ($(LOG_SERIAL_TO_FILE), yes)
+	minicom -D $(SERIAL_PORT) -b $(SERIAL_BAUD) -C serial.log
+else
 	minicom -D $(SERIAL_PORT) -b $(SERIAL_BAUD)
+endif
 
 monitor:
+ifeq ($(LOG_SERIAL_TO_FILE), yes)
+	minicom -D $(SERIAL_PORT) -b $(SERIAL_BAUD) -C serial.log
+else
 	minicom -D $(SERIAL_PORT) -b $(SERIAL_BAUD)
+endif
 
 print-%: ; @echo $* = $($*)
 
