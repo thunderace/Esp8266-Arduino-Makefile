7ad216321a83e36d267c9542645ce749647a8f0e
Mathew <mathew@winters.org.nz>
2018-08-28 14:41:46 +1200

    Work with 2.4.2 (and later) and fix scripts conditions.

--- esp8266-install-from-git.sh
+++ esp8266-install-from-git.sh
@@ -2,7 +2,8 @@
 git clone https://github.com/esp8266/Arduino.git esp8266.git
 
 cd esp8266.git/tools && ./get.py && cd ../..
-if [[ "$OSTYPE" == "cygwin" ]]; then
+if [ "$OSTYPE" = "cygwin" ]
+then
 	chmod +x ./esp8266.git/tools/esptool/esptool.exe
 	chmod +x ./esp8266.git/tools/mkspiffs/mkspiffs.exe
 fi
--- esp8266-install.sh
+++ esp8266-install.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-declare ESP8266_VER=2.4.1
+ESP8266_VER=2.4.2
 
-declare DOWNLOAD_CACHE=./download
+DOWNLOAD_CACHE=./download
 mkdir $DOWNLOAD_CACHE
 
-# Get Arduino core for ESP8266 chip
+Get Arduino core for ESP8266 chip
 wget --no-clobber https://github.com/esp8266/Arduino/releases/download/$ESP8266_VER/esp8266-$ESP8266_VER.zip -P $DOWNLOAD_CACHE
 unzip $DOWNLOAD_CACHE/esp8266-$ESP8266_VER.zip
 mkdir esp8266-$ESP8266_VER/package
 wget --no-clobber http://arduino.esp8266.com/versions/$ESP8266_VER/package_esp8266com_index.json -O esp8266-$ESP8266_VER/package/package_esp8266com_index.template.json
 cd esp8266-$ESP8266_VER/tools && ./get.py && cd ../..
-if [ "$OSTYPE" == "cygwin" ] || [ "$OSTYPE" == "msys" ]; then
+if [ "$OSTYPE" = "cygwin" ] || [ "$OSTYPE" = "msys" ]
+then
 	chmod +x ./esp8266-$ESP8266_VER/tools/esptool/esptool.exe
 	chmod +x ./esp8266-$ESP8266_VER/tools/mkspiffs/mkspiffs.exe
 fi
